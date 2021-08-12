2ce04634bcbb06b62c24c8e51d4427680fe4ba03
wyk <903871606@qq.com>
2021-08-12 23:45:36 +0800

    dd

--- src/renderer/components/LandingPage/SystemInformation.vue
+++ src/renderer/components/LandingPage/SystemInformation.vue
@@ -31,6 +31,11 @@
     图标
     <span class="icon-view"></span>
     <span class="icon-user"></span>
+    <el-tag>标签一</el-tag>
+    <el-tag type="success">标签二</el-tag>
+    <el-tag type="info">标签三</el-tag>
+    <el-tag type="warning">标签四</el-tag>
+    <el-tag type="danger">标签五</el-tag>
   </div>
 </template>
 
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -7,7 +7,7 @@ import store from './store'
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import '@/icomoon/style.css'
+// import '@/icomoon/style.css'
 import '@/style/index.css'
 
 Vue.use(ElementUI)
